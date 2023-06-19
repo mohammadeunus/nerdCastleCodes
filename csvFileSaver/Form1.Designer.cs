6a47073011a5a9499158017005d6c2377135f699
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-06-19 13:24:47 +0600

    changed design, added fileExistance checker to add header in the csv file

--- csvFileSaver/Form1.Designer.cs
+++ csvFileSaver/Form1.Designer.cs
@@ -28,123 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBoxEmail = new TextBox();
-            textBoxName = new TextBox();
-            textBoxContact = new TextBox();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            button1 = new Button();
-            textBox1 = new TextBox();
-            label1 = new Label();
-            button2 = new Button();
-            SuspendLayout();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxContact = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.process1 = new System.Diagnostics.Process();
+            this.SuspendLayout();
             // 
             // textBoxEmail
             // 
-            textBoxEmail.Location = new Point(141, 82);
-            textBoxEmail.Name = "textBoxEmail";
-            textBoxEmail.Size = new Size(138, 23);
-            textBoxEmail.TabIndex = 0;
+            this.textBoxEmail.Location = new System.Drawing.Point(141, 82);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(138, 23);
+            this.textBoxEmail.TabIndex = 0;
             // 
             // textBoxName
             // 
-            textBoxName.Location = new Point(141, 53);
-            textBoxName.Name = "textBoxName";
-            textBoxName.Size = new Size(138, 23);
-            textBoxName.TabIndex = 0;
+            this.textBoxName.Location = new System.Drawing.Point(141, 53);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(138, 23);
+            this.textBoxName.TabIndex = 0;
             // 
             // textBoxContact
             // 
-            textBoxContact.Location = new Point(141, 111);
-            textBoxContact.Name = "textBoxContact";
-            textBoxContact.Size = new Size(138, 23);
-            textBoxContact.TabIndex = 0;
+            this.textBoxContact.Location = new System.Drawing.Point(141, 111);
+            this.textBoxContact.Name = "textBoxContact";
+            this.textBoxContact.Size = new System.Drawing.Size(138, 23);
+            this.textBoxContact.TabIndex = 0;
             // 
             // label2
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(27, 56);
-            label2.Name = "label2";
-            label2.Size = new Size(39, 15);
-            label2.TabIndex = 1;
-            label2.Text = "Name";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
             // 
             // label3
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(27, 85);
-            label3.Name = "label3";
-            label3.Size = new Size(36, 15);
-            label3.TabIndex = 1;
-            label3.Text = "Email";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Email";
             // 
             // label4
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(27, 114);
-            label4.Name = "label4";
-            label4.Size = new Size(93, 15);
-            label4.TabIndex = 1;
-            label4.Text = "ContactNumber";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "ContactNumber";
             // 
             // button1
             // 
-            button1.BackColor = SystemColors.ActiveCaption;
-            button1.Location = new Point(27, 182);
-            button1.Name = "button1";
-            button1.Size = new Size(252, 23);
-            button1.TabIndex = 2;
-            button1.Text = "Enter";
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += insertDataInCSV;
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(27, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.buttonInsertDataInCSV);
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(456, 77);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(138, 23);
-            textBox1.TabIndex = 0;
+            this.textBox1.Location = new System.Drawing.Point(456, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 23);
+            this.textBox1.TabIndex = 0;
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(473, 53);
-            label1.Name = "label1";
-            label1.Size = new Size(99, 15);
-            label1.TabIndex = 1;
-            label1.Text = "Search For Emails";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(473, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Search For Emails";
             // 
             // button2
             // 
-            button2.BackColor = SystemColors.ActiveCaption;
-            button2.Location = new Point(401, 114);
-            button2.Name = "button2";
-            button2.Size = new Size(252, 23);
-            button2.TabIndex = 2;
-            button2.Text = "Search";
-            button2.UseVisualStyleBackColor = false;
-            button2.Click += insertDataInCSV;
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(401, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(252, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardInputEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(735, 302);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label1);
-            Controls.Add(label2);
-            Controls.Add(textBox1);
-            Controls.Add(textBoxName);
-            Controls.Add(textBoxContact);
-            Controls.Add(textBoxEmail);
-            Name = "Form1";
-            Text = "Enter";
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 302);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxContact);
+            this.Controls.Add(this.textBoxEmail);
+            this.Name = "Form1";
+            this.Text = "Enter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
@@ -159,5 +171,6 @@
         private TextBox textBox1;
         private Label label1;
         private Button button2;
+        private System.Diagnostics.Process process1;
     }
 }
--- csvFileSaver/Form1.cs
+++ csvFileSaver/Form1.cs
@@ -3,30 +3,37 @@ namespace csvFileSaver
     public partial class Form1 : Form
     {
         public string Name { get; set; }
-        public string Email { get; set; }
-        public int contactNumber { get; set; }
-        public object ContactNumber { get; private set; }
+        public string Email { get; set; } 
+        public string ContactNumber { get; set; }
 
         public Form1()
         {
             InitializeComponent();
 
         }
-
-        private void insertDataInCSV(object sender, EventArgs e)
+          
+        private void buttonInsertDataInCSV(object sender, EventArgs e)
         {
             Name = textBoxName.Text;
             Email = textBoxEmail.Text;
-            contactNumber = Int32.Parse(textBoxContact.Text);
+            ContactNumber = textBoxContact.Text;
 
             // Create a CSV file path in the desired directory
-            string directoryPath = @"F:\aspNetEunus";
+            string directoryPath = @"E:\project\nerdCastle";
             string fileName = "data.csv";
             string filePath = Path.Combine(directoryPath, fileName);
 
+            bool checkFileExistence = File.Exists(filePath);
             // Write the data to the CSV file
             using (StreamWriter writer = new StreamWriter(filePath, true))
             {
+                //// Check if the CSV file already exists
+                if (!checkFileExistence)
+                {
+                    string csvHeaders = "Name,Email,Contact";
+                    writer.WriteLine(csvHeaders);
+                }
+
                 // Create a string with the property values separated by commas
                 string csvLine = $"{Name},{Email},{ContactNumber}";
 
