be9c2c6f6b10cf6280d06367ae7132b8a81fc526
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-09-17 12:52:58 +0600

    working

--- SurveyAssignment/SurveyAssignment.web/Models/ResponseModel.cs
+++ SurveyAssignment/SurveyAssignment.web/Models/ResponseModel.cs
@@ -1,8 +1,8 @@
-﻿namespace SurveyAssignment.web.Entities;
+﻿namespace SurveyAssignment.web.Models;
 
 public class ResponseModel
-{ 
-    public int Id { get; set; } 
+{
+    public int Id { get; set; }
     public string Option { get; set; }
     public string UserIpAddress { get; set; } // Add this field to store the user's IP address
 }
